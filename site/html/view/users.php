ef2f462c31519c3afb945485106db8776de029ad
Insuline13 <alexandra.cerottini@heig-vd.ch>
2021-10-06 22:19:09 +0200

    commencement de users

--- site/html/view/users.php
+++ site/html/view/users.php
@@ -16,12 +16,45 @@ if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] != true) {
 
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>PHP Test</title>
+    <link rel="stylesheet" type="text/css" href="css/inbox.css"/>
 </head>
 <body>
-<?php echo '<p>Users management</p>';
-?>
+<div class="container">
+    <h2 class="title">User management</h2>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Email</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        if (isset($file_db)) {
+            $sql = $file_db->prepare("SELECT * FROM users");
+        }
+        $sql->execute();
+        foreach ($sql->fetchAll() as $user) {
+            ?>
+            <tr>
+                <td><?=$user['email']?></td>
+                <td><?=$user['is_activ'] ? "active" : "inactive"?></td>
+                <td><?=$user['is_admin'] ? "admin" : "collaborator"?></td>
+                <td><a href="./user/modify_user.php?email=<?=$user['email']?>">Modify</a></td>
+                <td><a href="./user/delete_user.php?email=<?=$user['email']?>">Delete</a></td>
+            </tr>
+            <?php
+        }
+        ?>
+        </tbody>
+    </table>
+    <a href="./user/new_user.php" class="btn btn-dark">Add user</a>
+</div>
 </body>
 </html>
