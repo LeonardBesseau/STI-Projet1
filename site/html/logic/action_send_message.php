cc624ebb4678e563303bec54742c05e96d70ced0
Insuline13 <alexandra.cerottini@heig-vd.ch>
2021-10-13 00:10:37 +0200

    résolu problème avec apostrophes

--- site/html/logic/action_send_message.php
+++ site/html/logic/action_send_message.php
@@ -12,7 +12,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // get variables
     $recipient = $_POST['recipient'];
     $subject = $_POST['subject'];
+    $subject = str_replace("'", "''", $subject);
     $body = $_POST['body'];
+    $body = str_replace("'", "''", $body);
     date_default_timezone_set('Europe/Zurich');
     $date = date('d/m/Y h:i:s', time());
 
@@ -22,6 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $sql = $file_db->prepare("INSERT INTO messages (subject, body, sender, recipient, date) VALUES ('$subject', '$body', '$sender', '$recipient', '$date')");
             $sql->execute();
             //redirect to inbox
+
             header('Location: ../view/inbox.php');
         } else {
             echo 'Error: unable to send message';
